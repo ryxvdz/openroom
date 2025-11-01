31bb5e081d87a9a9bde59a59ddfcc8b0b7dd3572
Ryan Dias <ryanvicd2112@gmail.com>
2025-11-01 12:02:58 -0300

    update: archives of springboot

---
+++ openroom/src/main/java/com/example/openroom/OpenroomApplication.java
@@ -0,0 +1,13 @@
+package com.example.openroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenroomApplication.class, args);
+	}
+
+}
---
+++ openroom/src/test/java/com/example/openroom/OpenroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
